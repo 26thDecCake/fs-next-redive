f392c7475d438f08278e900bfe278fb09965e979
Romi Than <rrromithan@gmail.com>
2024-11-25 14:08:34 +0700

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
@@ -54,10 +55,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
+      <body className={workSans.variable}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -8,5 +8,6 @@ export const structure: StructureResolver = (S) =>
       [
         S.documentTypeListItem("author").title("Authors"),
         S.documentTypeListItem("startup").title("Startups"),
+        S.documentTypeListItem("playlist").title("Playlists"),
       ]
     )
